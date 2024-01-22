c168fb65e4f8e5decec8b871ad133977b31adda4
hientranngoc <ngochien10esbk@gmail.com>
2024-01-23 07:06:32 +0800

    increase decrease

--- src/pages/Cart/CartIem.jsx
+++ src/pages/Cart/CartIem.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { decreaseItem, increaseItem } from '../../redux/reducer/CartReducer';
 
 function CartIem({item}) {
+    let dispatch = useDispatch()
+    let totalPrice = item.quantity * item.price
     return (
         <div className='container row border'>
             <img className='col-md-3' src={item.image} alt=''/>
             <div className='col-md-9'>
             <div className='row align-items-center mb-2'>
                 <h4 className='col-md-6 text text-start'>{item.name}</h4>
-                <p className='col-md-6 text-end'>{item.price}</p>
+                <p className='col-md-6 text-end'>{totalPrice}</p>
             </div>
             <div className='row border'>
             <div className='row col align-items-center'>
-                <button className='btn btn-primary col'><i class="fa-solid fa-plus"></i></button>
-                <p className="col text-center">1</p>
-                <button className='btn btn-primary col'><i class="fa-solid fa-minus"></i></button>
+                <button className='btn btn-primary col'onClick={()=> dispatch(increaseItem(item.id))}><i class="fa-solid fa-plus"></i></button>
+                <p className="col text-center">{item.quantity}</p>
+                <button className='btn btn-primary col'onClick={()=> dispatch(decreaseItem(item.id))}><i class="fa-solid fa-minus"></i></button>
             </div>
             <div className='row col'>
                 <button type="button" className='btn btn-outline-info col'>Edit</button>
--- src/redux/reducer/CartReducer.jsx
+++ src/redux/reducer/CartReducer.jsx
@@ -11,12 +11,12 @@ const CartReducer = createSlice({
     reducers: {
         addToCart: (state, action) => {
             let payload = action.payload
-            console.log("test")
+            
           // use findIndex to search for duplicate shoe
             let index = state.cartShoes.findIndex((item)=>{                
                 return item.id === payload.id
-        });
-        console.log(index)
+            });
+            console.log(index)
             let newCartShoe = [...state.cartShoes]
             if (index === -1) {
                 let addedCartShoe = {...payload, quantity:1}
@@ -26,11 +26,35 @@ const CartReducer = createSlice({
             }
             console.log("Updated state:", { ...state, cartShoes: newCartShoe });
             return {...state, cartShoes: newCartShoe}
-    }},
+        },
+        increaseItem: (state,action) => {
+            let itemID = action.payload
+            // console.log("itemID", itemID)
+            let index = state.cartShoes.findIndex((item)=>{
+                return item.id === itemID
+            })
+            // console.log("index",index)
+            let newCartShoe = [...state.cartShoes]
+            newCartShoe[index] = {...newCartShoe[index], quantity: newCartShoe[index].quantity + 1}
+
+            return {...state, cartShoes: newCartShoe}
+        },
+        decreaseItem: (state, action) => {
+            let itemID = action.payload
+            let index = state.cartShoes.findIndex((item)=>{
+                return item.id === itemID
+            })
+            let newCartShoe = [...state.cartShoes]
+            newCartShoe[index] = {...newCartShoe[index], quantity: newCartShoe[index].quantity - 1}
+            // if else in decrease
+
+            return {...state, cartShoes: newCartShoe}
+        },
+    },
 
 });
 
-export const {addToCart} = CartReducer.actions;
+export const {addToCart, increaseItem, decreaseItem} = CartReducer.actions;
 
 export default CartReducer.reducer;
 
